2d8bc65671823d2f5ff0131a6d0300135cea05b7
hermine-dm <herdementh@gmail.com>
2019-11-25 12:27:51 +0100

    [finish]model/migration/validation linked to Item ok

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,13 @@
 class Item < ApplicationRecord
+	validates :title,
+		presence: true,
+		length: { in: 3..15, wrong_length: "3 to 15 characters is allowed" }
+	validates :price,
+		presence: true, numericality: { greater_than: 0 }
+	validates :description,
+		presence: true,
+		length: { in: 5..100, wrong_length: "5 to 100 characters is allowed" }
+	validates :image_url,
+		presence: true
+
 end
