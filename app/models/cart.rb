4bb4f62570a54de1afd4453fbdcfc3594263f456
philippegatti <ph.gatti@gmail.com>
2019-11-25 22:18:35 +0100

    back show Cart created + destroy items from cart + seeds reviewed

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -2,4 +2,10 @@ class Cart < ApplicationRecord
 	belongs_to :user
 	has_many :join_table_carts_items
 	has_many :items, through: :join_table_carts_items
+
+
+	def total_price
+		self.items.to_a.map{|item| item.price }.inject(0, :+)
+	end
+	
 end
