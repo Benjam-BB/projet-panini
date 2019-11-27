f2eba8d755ebbaab9f9f893eca4680a16fa982dd
philippegatti <ph.gatti@gmail.com>
2019-11-27 14:27:21 +0100

    flash messages + redirect_to when you add or delete item in your cart

--- app/controllers/join_table_carts_items_controller.rb
+++ app/controllers/join_table_carts_items_controller.rb
@@ -3,8 +3,11 @@ class JoinTableCartsItemsController < ApplicationController
 		
 	def create
 		JoinTableCartsItem.create(item_id:params[:item_id],cart_id:Cart.find_by(user_id:current_user.id).id)
+		flash[:success] = 'La carte a été ajoutée à votre panier'
 	end
 	def destroy
 		JoinTableCartsItem.find(params[:id]).delete
+		redirect_to user_cart_path(current_user.id)
+		flash[:success] = 'La carte a été retirée de votre panier'
 	end
 end
